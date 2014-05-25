e154bfaf06f76bc8ae601625932078c660428610
SaySarah <Saysarah@gmail.com>
2014-05-24 21:43:15 -0400

    Modification and upload of cachematrix.R from local PC

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The Two functions below cache the inverse of a matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##makeCacheMatrix() creates a special "matrix" object to cache its inverse. 
+makeCacheMatrix <- function(x = matrix()) { 
+ 
+    i <- NULL
+    setMatrix <- function(y){   ## set the value of the matrix
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x         ## get the original value of the matrix
+    setInverse <- function(solve) i <<- solve ## set the inverse matrix 
+    getInverse <- function() i  ## get the value of the inverser matrix
+    
+    # Group functions within this object and sets datafield
+    list(setMatrix = setMatrix, get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)   
 }
 
 
-## Write a short comment describing this function
+## The CacheSolve() function computes & returns the inverse of the special  
+## "matrix" returned by the makeCacheMatrix() above
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+   
+    i <- x$getInverse() ## calculate the inverse of matrix in makeCacheMatrix() 
+    if(!is.null(i)){    ## first checks if inverse matrix already calculated
+        message("getting cached inverse")
+        return(i)       ## returns the inverse of the "matrix
+    }
+    matrix <- x$get()   ## Otherwise, if not stored in makeCacheMatrix() 
+    i <- solve(matrix, ...)     ## calculates inverse of the matrix
+    x$setInverse(i)     ##sets value of inverse matrix in cache via setInverse()
+    i
 }
+    
+## ginv() can be used like solve() to find the inverse of a matrix
+## Note that use %*% for matrix multiplication 
